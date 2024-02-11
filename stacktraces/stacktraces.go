18374c0ab1bd126961842e7a23cfe0d106d25546
parasaurolophus <apps@rader.us>
2024-02-11 07:37:01 -0600

    Refactor source files

--- stacktraces/stacktraces.go
+++ stacktraces/stacktraces.go
@@ -3,6 +3,9 @@
 package stacktraces
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"runtime"
 )
 
@@ -135,4 +138,148 @@ func ShortStackTrace(skipFrames any) string {
 
 	_, trace := formatStackTrace(skipFrames, nil, shortFrameFormatter())
 	return trace
+}
+
+type (
+
+	// Type of function used to write a single frame to a stack trace.
+	stackFrameFormatter func(writer *bufio.Writer, frameNumber int, frame *runtime.Frame)
+
+	// Type of function used to test a frame for inclusion in a stack trace.
+	stackFrameTest func(frame *runtime.Frame) bool
+)
+
+// Return a function which writes a multi-line representation of a stack frame.
+func longFrameFormatter() stackFrameFormatter {
+
+	writeSeparator := false
+
+	return func(writer *bufio.Writer, frameNumber int, frame *runtime.Frame) {
+
+		if writeSeparator {
+			writer.WriteString("---\n")
+		} else {
+			writeSeparator = true
+		}
+
+		writer.WriteString(
+			fmt.Sprintf(
+				"%d:%s\n%s:%d\n%#v\n",
+				frameNumber,
+				frame.Function,
+				frame.File,
+				frame.Line,
+				frame.PC))
+	}
+}
+
+// Return a function which writes a one-line representation of a stack frame.
+func shortFrameFormatter() stackFrameFormatter {
+
+	writeSeparator := false
+
+	return func(writer *bufio.Writer, frameNumber int, frame *runtime.Frame) {
+
+		if writeSeparator {
+			writer.WriteString(" < ")
+		} else {
+			writeSeparator = true
+		}
+
+		writer.WriteString(
+			fmt.Sprintf(
+				"%d:%s [%s:%d]",
+				frameNumber,
+				frame.Function,
+				frame.File,
+				frame.Line))
+	}
+}
+
+// Helper used by New(string, any), LongStackTrace(any) and
+// ShortStackTrace(any).
+//
+// Returns long and short string representations of the current call stack.
+func formatStackTrace(skipFrames any, longFormatter stackFrameFormatter, shortFormatter stackFrameFormatter) (string, string) {
+
+	longBuffer := bytes.Buffer{}
+	longWriter := bufio.NewWriter(&longBuffer)
+
+	shortBuffer := bytes.Buffer{}
+	shortWriter := bufio.NewWriter(&shortBuffer)
+
+	skip := 0
+	frameTest := func(*runtime.Frame) bool { return true }
+
+	switch v := skipFrames.(type) {
+
+	case int:
+		if v < 0 {
+			// the number of frames to skip is empirically derived and may
+			// change any time the code in this library is refactored
+			skip = 3
+		} else {
+			skip = v
+		}
+
+	case string:
+		frameTest = startWhenSeen(v)
+
+	default:
+		// the number of frames to skip is empirically derived and may
+		// change any time the code in this library is refactored
+		skip = 3
+	}
+
+	pc := make([]uintptr, 1024)
+	n := runtime.Callers(skip, pc)
+
+	if n < 1 {
+		return "", ""
+	}
+
+	pc = pc[:n]
+	frames := runtime.CallersFrames(pc)
+	frameNumber := skip
+
+	for {
+
+		frame, more := frames.Next()
+
+		if frameTest(&frame) {
+
+			if longFormatter != nil {
+				longFormatter(longWriter, frameNumber, &frame)
+			}
+
+			if shortFormatter != nil {
+				shortFormatter(shortWriter, frameNumber, &frame)
+			}
+		}
+
+		frameNumber += 1
+
+		if !more {
+			break
+		}
+	}
+
+	longWriter.Flush()
+	shortWriter.Flush()
+	return longBuffer.String(), shortBuffer.String()
+}
+
+// Return a function that returns true when invoked for a frame with the given
+// function name and all that follow it.
+//
+// This is the frame test used by formatStackTrace() when skipFrames is a string
+func startWhenSeen(startWhen string) stackFrameTest {
+
+	seen := false
+
+	return func(frame *runtime.Frame) bool {
+
+		seen = seen || startWhen == frame.Function
+		return seen
+	}
 }
