4866d86e9c1ca8f13db41029968368af80b78fd7
parasaurolophus <apps@rader.us>
2024-03-15 09:25:33 -0500

    Improve example.go

--- example/example.go
+++ example/example.go
@@ -9,25 +9,24 @@ import (
 	"parasaurolophus/go/stacktraces"
 )
 
-// Called from main() to demonstrate logging panics.
-func deliberatePanic() {
-	panic("deliberate")
-}
-
-// Writes three (not four) log entries to stdout and exits with status code 1
-// due to a deliberately caused panic.
-//
-// {"time":"2024-03-08T08:18:15.535425196-06:00","verbosity":"TRACE","msg":"you will see this","counter":0,"file":{"function":"main.main","file":"/source/go/example/example.go","line":116},"tags":["EXAMPLE"]}
-// {"time":"2024-03-08T08:18:15.536047625-06:00","verbosity":"ALWAYS","msg":"recovered: deliberate","counter":1,"recovered":"deliberate","file":{"function":"main.deliberatePanic","file":"/source/go/example/example.go","line":14},"stacktrace":"5:main.main.func1 [/source/go/example/example.go:81] < 6:runtime.gopanic [/usr/local/go/src/runtime/panic.go:770] < 7:main.deliberatePanic [/source/go/example/example.go:14] < 8:main.main [/source/go/example/example.go:123] < 9:runtime.main [/usr/local/go/src/runtime/proc.go:271] < 10:runtime.goexit [/usr/local/go/src/runtime/asm_arm64.s:1222]","tags":["EXAMPLE","PANIC"]}
-// {"time":"2024-03-08T08:18:15.536090403-06:00","verbosity":"TRACE","msg":"exiting main.main","counter":1,"tags":["EXAMPLE"]}
-func main() {
-
-	// Get the calling function's name -- main.main in this case.
-	functionName := stacktraces.FunctionName()
+var (
 
 	// An additional value that will be included in every log entry using
 	// loggerOptions.BaseAttributes.
-	counter := 0
+	counter = 0
+
+	// Make main.main()'s source info available globally for logging.
+	sourceInfo stacktraces.SourceInfo
+)
+
+// Writes two (not three) log entries to stdout and exits with status code 2 due
+// to a deliberately caused panic.
+//
+//	{"time":"2024-03-15T04:31:00.30668337-05:00","verbosity":"TRACE","msg":"you will see this","counter":0,"file":{"function":"main.main","file":"/source/go/example/example.go","line":58},"tags":["EXAMPLE"]}
+//	{"time":"2024-03-15T04:31:00.306936205-05:00","verbosity":"ALWAYS","msg":"recovered: runtime error: invalid memory address or nil pointer dereference","counter":1,"recovered":"runtime error: invalid memory address or nil pointer dereference","file":{"function":"runtime.panicmem","file":"/usr/local/go/src/runtime/panic.go","line":261},"stacktrace":"5:main.finally [/source/go/example/example.go:96] < 6:runtime.gopanic [/usr/local/go/src/runtime/panic.go:770] < 7:runtime.panicmem [/usr/local/go/src/runtime/panic.go:261] < 8:runtime.sigpanic [/usr/local/go/src/runtime/signal_unix.go:881] < 9:main.uncheckedPointer [/source/go/example/example.go:70] < 10:main.main [/source/go/example/example.go:65] < 11:runtime.main [/usr/local/go/src/runtime/proc.go:271] < 12:runtime.goexit [/usr/local/go/src/runtime/asm_arm64.s:1222]","tags":["EXAMPLE","PANIC"]}
+func main() {
+
+	_, sourceInfo, _ = stacktraces.FunctionInfo(nil)
 
 	// Configuration options for a logging.Logger.
 	loggerOptions := logging.LoggerOptions{
@@ -43,52 +42,11 @@ func main() {
 		BaseAttributes: []any{"counter", &counter},
 	}
 
-	// Create a synchronous logger.
-	logger := logging.New(os.Stdout, &loggerOptions)
+	// Create an asynchronous logger.
+	logger := logging.NewAsync(os.Stdout, &loggerOptions)
 
-	//////////////////////////////////////////////////////////////////////////
-	// ALWAYS log panics with stack trace & recovered attributes in addition to
-	// TRACE logging every exit from main().
-	defer func() {
-
-		// Check to see if a panic occurred.
-		r := recover()
-
-		// Always log a stack trace, source info, and recovered value when a
-		// panic occurs.
-		if r != nil {
-
-			// Increment counter.
-			counter += 1
-
-			// Note that counter is 1 in this entry and subsequent entry.
-			logger.Always(
-				func() string {
-					return fmt.Sprintf("recovered: %v", r)
-				},
-				logging.STACKTRACE, nil,
-				logging.RECOVERED, r,
-				logging.TAGS, []string{logging.PANIC},
-				logging.FILE, logging.FILE_SKIPFRAMES_FOR_PANIC,
-			)
-		}
-
-		logger.Trace(
-			func() string {
-				return fmt.Sprintf("exiting %s", functionName)
-			})
-
-		// logger.Stop is a no-op for synchronous logger, but called here as
-		// insurance if we ever decide to change to asyncrhonous logging.
-		logger.Stop()
-
-		// Signal an unsuccessful status when a panic occurs.
-		if r != nil {
-			os.Exit(2)
-		}
-	}()
-	// end of defer
-	//////////////////////////////////////////////////////////////////////////
+	// Log every exit from main.
+	defer finally(logger)
 
 	// Default verbosity is FINE, so logger.Trace() does not write an entry.
 	logger.Trace(func() string { return "you won't see this" })
@@ -105,5 +63,54 @@ func main() {
 		logging.FILE, logging.FILE_SKIPFRAMES_FOR_CALLER)
 
 	// Deliberately cause a panic so as to demonstrate deferred logging.
-	deliberatePanic()
+	uncheckedPointer(nil)
+}
+
+// Called from main() to demonstrate logging panics.
+func uncheckedPointer(p *int) {
+	fmt.Println(*p)
+}
+
+// Invoked by main() using defer to ensure logging on exit, even if a panic
+// occurs.
+func finally(logger logging.Logger) {
+
+	// Check to see if a panic occurred.
+	r := recover()
+
+	if r == nil {
+
+		// Exiting normally, so log at TRACE level.
+		logger.Trace(
+			func() string {
+				return fmt.Sprintf("exiting %s", sourceInfo.Function)
+			})
+
+	} else {
+
+		// Always log a stack trace, source info, and recovered value when a
+		// panic occurs.
+
+		// Increment counter.
+		counter += 1
+
+		// Note that counter is 1 in this entry.
+		logger.Always(
+			func() string {
+				return fmt.Sprintf("recovered: %v", r)
+			},
+			logging.STACKTRACE, nil,
+			logging.RECOVERED, r,
+			logging.TAGS, []string{logging.PANIC},
+			logging.FILE, logging.FILE_SKIPFRAMES_FOR_PANIC,
+		)
+	}
+
+	// Calling Stop() allows for a clean shutdown of an asynchronous logger.
+	logger.Stop()
+
+	// Signal an unsuccessful exit status when a panic has occurred.
+	if r != nil {
+		os.Exit(2)
+	}
 }
