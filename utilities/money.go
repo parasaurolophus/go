f573688e4a18e975839aee21289c9042c23c8408
parasaurolophus <apps@rader.us>
2024-05-11 05:51:14 -0500

    Add Money type to utilities library

---
+++ utilities/money.go
@@ -0,0 +1,78 @@
+package utilities
+
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
+type (
+	Money struct {
+		value  float64
+		digits int
+	}
+)
+
+func New(value float64, digits int) Money {
+	if digits < 0 {
+		digits = 0
+	}
+	return Money{
+		value:  value,
+		digits: digits,
+	}
+}
+
+func (m Money) MarshalJSON() ([]byte, error) {
+	return []byte(m.String()), nil
+}
+
+func (m *Money) Scan(state fmt.ScanState, _ rune) error {
+	token, err := state.Token(true, m.tokenizer())
+	if err != nil {
+		return err
+	}
+	f, err := strconv.ParseFloat(string(token), 64)
+	if err != nil {
+		return err
+	}
+	m.value = f
+	return nil
+}
+
+func (m Money) String() string {
+	f := "%." + strconv.Itoa(m.digits) + "f"
+	return fmt.Sprintf(f, m.Value())
+}
+
+func (m *Money) UnmarshalJSON(b []byte) error {
+	var f float64
+	_, err := fmt.Sscan(string(b), &f)
+	if err == nil {
+		m.value = f
+	}
+	return err
+}
+
+func (m Money) Value() float64 {
+	return m.value
+}
+
+func (m Money) tokenizer() func(rune) bool {
+	firstRune := true
+	decimalPointSeen := false
+	return func(r rune) bool {
+		defer func() { firstRune = false }()
+		if r == '-' {
+			return firstRune
+		}
+		if r == '.' {
+			if decimalPointSeen {
+				return false
+			}
+			decimalPointSeen = true
+			return true
+		}
+		return unicode.IsDigit(r)
+	}
+}
---
+++ utilities/money_test.go
@@ -0,0 +1,110 @@
+package utilities
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestMarshalJSON(t *testing.T) {
+	type S struct {
+		M Money `json:"m,omitempty"`
+	}
+	s := S{
+		M: New(4.2, 2),
+	}
+	b, err := json.Marshal(&s)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(b) != `{"m":4.20}` {
+		t.Errorf(`expected {"m":4.20}, got %s`, string(b))
+	}
+	s = S{
+		M: New(4.2, 0),
+	}
+	b, err = json.Marshal(&s)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(b) != `{"m":4}` {
+		t.Errorf(`expected {"m":4}, got %s`, string(b))
+	}
+}
+
+func TestScan(t *testing.T) {
+	m1 := New(0.0, 2)
+	m2 := New(0.0, 2)
+	n, err := fmt.Sscan(" 4.20 -0.01 ", &m1, &m2)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if n != 2 {
+		t.Errorf("expected 2, got %d", n)
+	}
+	if m1.Value() != 4.2 {
+		t.Errorf("expected 4.2, got %f", m1.Value())
+	}
+	if m2.Value() != -0.01 {
+		t.Errorf("expected -0.01, got %f", m1.Value())
+	}
+	n, err = fmt.Sscan("12.34.", &m1)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if n != 1 {
+		t.Errorf("expected 1, got %d", n)
+	}
+	if m1.Value() != 12.34 {
+		t.Errorf("expected 12.34, got %f", m1.Value())
+	}
+	n, err = fmt.Sscan("567.80-", &m1)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if n != 1 {
+		t.Errorf("expected 1, got %d", n)
+	}
+	if m1.Value() != 567.8 {
+		t.Errorf("expected 567.8, got %f", m1.Value())
+	}
+}
+
+func TestString(t *testing.T) {
+	m := New(78.9, 2)
+	s := m.String()
+	if s != "78.90" {
+		t.Errorf(`expected "78.90", got "%s"`, s)
+	}
+	m = New(78.9, 1)
+	s = m.String()
+	if s != "78.9" {
+		t.Errorf(`expected "78.9", got "%s"`, s)
+	}
+	m = New(78.9, -1)
+	s = m.String()
+	if s != "79" {
+		t.Errorf(`expected "79", got "%s"`, s)
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	type S struct {
+		M Money `json:"m,omitempty"`
+	}
+	var s S
+	err := json.Unmarshal([]byte(`{}`), &s)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if s.M.Value() != 0.0 {
+		t.Errorf("expected 0.0, got %f", s.M.Value())
+	}
+	err = json.Unmarshal([]byte(`{"m": 4.200}`), &s)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if s.M.Value() != 4.2 {
+		t.Errorf("expected 4.2, got %f", s.M.Value())
+	}
+}
