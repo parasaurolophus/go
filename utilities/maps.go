02397b8ca2303068b5e9856dd4853c8a5bae4d61
parasaurolophus <apps@rader.us>
2024-07-05 12:28:04 -0500

    Add convenience functions for accessing maps

--- utilities/maps.go
+++ utilities/maps.go
@@ -3,7 +3,11 @@
 package utilities
 
 import (
+	"fmt"
 	"slices"
+	"strconv"
+
+	"github.com/google/uuid"
 )
 
 // Return a shallow copy of m, with only the keys specified by keepKeys.
@@ -15,6 +19,62 @@ func FilterMapKeys[K comparable, V any](m map[K]V, keepKeys ...K) map[K]V {
 		}
 	}
 	return result
+}
+
+// Interpret the value of k in m as a float64, signalling an error if k is
+// missing or of an unupported type.
+func GetRequiredFloat(k string, m map[string]any) (float64, error) {
+	return GetRequiredValue(k, m, func(s string) (float64, error) {
+		return strconv.ParseFloat(s, 64)
+	})
+}
+
+// Interpret the value of k in m as a string, signalling an error if k is
+// missing or of an unupported type.
+func GetRequiredString[K comparable](k K, m map[K]any) (string, error) {
+	// note that the parser function will not be called in this circumstance
+	return GetRequiredValue[K, string](k, m, nil)
+}
+
+// Interpret the value of k in m as an uuid.UUID, signalling an error if k is
+// missing or of an unupported type.
+func GetRequiredUUID[K comparable](k K, m map[K]any) (uuid.UUID, error) {
+	return GetRequiredValue(k, m, uuid.Parse)
+}
+
+// Interpret the value of k in m as a V, signalling an error if k is missing or
+// of an unupported type.
+func GetRequiredValue[K comparable, V any](k K, m map[K]any, parser func(string) (V, error)) (value V, err error) {
+	v, found := m[k]
+	if !found {
+		err = fmt.Errorf("%v missing from %v", k, m)
+		return
+	}
+	switch x := v.(type) {
+	case V:
+		value = x
+	case string:
+		value, err = parser(x)
+	case fmt.Stringer:
+		value, err = parser(x.String())
+	default:
+		err = fmt.Errorf("%v, of type %T, cannot be interpreted as a %T", x, x, value)
+	}
+	return
+}
+
+// Cast the value of k in m to V, signalling an error if the value is not of
+// the specfied type.
+func GetValue[K comparable, V any](k K, m map[K]any) (value V, found bool, err error) {
+	v, found := m[k]
+	if !found {
+		return
+	}
+	value, ok := v.(V)
+	if !ok {
+		err = fmt.Errorf("expected %s to be of type %T, got %T", v, value, v)
+	}
+	return
 }
 
 // Return a map that combines the key / value pairs from all the given ones. The
--- utilities/maps_test.go
+++ utilities/maps_test.go
@@ -2,7 +2,11 @@
 
 package utilities
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
 
 func TestFilterMapKeys(t *testing.T) {
 
@@ -27,36 +31,157 @@ func TestFilterMapKeys(t *testing.T) {
 	}
 }
 
-func TestMergeMaps(t *testing.T) {
+func TestGetRequiredFloat(t *testing.T) {
+	m := map[string]any{
+		"field1": 4.2,
+		"field2": "value2",
+	}
+	f, err := GetRequiredFloat("field1", m)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if f != 4.2 {
+		t.Errorf("expected 4.2, got %g", f)
+	}
+	_, err = GetRequiredFloat("field2", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+	_, err = GetRequiredFloat("missing", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+}
 
+func TestGetRequiredString(t *testing.T) {
+	m := map[string]any{
+		"field1": "value1",
+		"field2": 42,
+	}
+	s, err := GetRequiredString("field1", m)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if s != "value1" {
+		t.Errorf(`expected "value1", got "%s"`, s)
+	}
+	_, err = GetRequiredString("field2", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+	_, err = GetRequiredString("missing", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+}
+
+type uuidString struct {
+	id uuid.UUID
+}
+
+func (u uuidString) String() string {
+	return u.id.String()
+}
+
+func TestGetRequiredUUID(t *testing.T) {
+	expected := uuid.New()
+	m := map[string]any{
+		"field1": expected,
+		"field2": expected.String(),
+		"field3": uuidString{id: expected},
+		"field4": 42,
+	}
+	actual, err := GetRequiredUUID("field1", m)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if actual != expected {
+		t.Errorf(`expected "%v", got "%v"`, expected, actual)
+	}
+	actual, err = GetRequiredUUID("field2", m)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if actual != expected {
+		t.Errorf(`expected "%v", got "%v"`, expected, actual)
+	}
+	actual, err = GetRequiredUUID("field3", m)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if actual != expected {
+		t.Errorf(`expected "%v", got "%v"`, expected, actual)
+	}
+	_, err = GetRequiredUUID("field4", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+	_, err = GetRequiredUUID("missing", m)
+	if err == nil {
+		t.Error("expected err not to be nil")
+	}
+}
+
+func TestGetValue(t *testing.T) {
+	m := map[int]any{
+		-1: "value -1",
+		2:  4.2,
+	}
+	s, found, err := GetValue[int, string](-1, m)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !found {
+		t.Fatalf("expected value for -1 to be found")
+	}
+	if s != "value -1" {
+		t.Errorf(`expected "value -1", got "%s"`, s)
+	}
+	f, found, err := GetValue[int, float64](2, m)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !found {
+		t.Fatalf("expected value for 2 to be found")
+	}
+	if f != 4.2 {
+		t.Errorf("expected 4.2, got %g", f)
+	}
+	_, found, err = GetValue[int, string](0, m)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if found {
+		t.Errorf("expected value for 0 not to be found")
+	}
+	_, _, err = GetValue[int, string](2, m)
+	if err == nil {
+		t.Errorf("expected err not to be nil")
+	}
+}
+
+func TestMergeMaps(t *testing.T) {
 	map1 := map[string]int{
 		"one": 1,
 		"two": 2,
 	}
-
 	map2 := map[string]int{
 		"three": 3,
 		"four":  4,
 	}
-
 	merged := MergeMaps(map1, map2)
-
 	if len(merged) != 4 {
 		t.Errorf("expected 4, got %d", len(merged))
 	}
-
 	if merged["one"] != 1 {
 		t.Errorf("expected 1, got %d", merged["one"])
 	}
-
 	if merged["two"] != 2 {
 		t.Errorf("expected 2, got %d", merged["two"])
 	}
-
 	if merged["three"] != 3 {
 		t.Errorf("expected 3, got %d", merged["three"])
 	}
-
 	if merged["four"] != 4 {
 		t.Errorf("expected 4, got %d", merged["four"])
 	}
